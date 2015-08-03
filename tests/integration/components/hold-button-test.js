cb9fa5dccbfc86f66a8071e0739209c2472d3a8d
Chris Sloey <chris@addjam.com>
2015-08-03 16:10:33 +0100

    Add integration test for hold-button component

--- tests/integration/components/hold-button-test.js
+++ tests/integration/components/hold-button-test.js
@@ -1,20 +1,19 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-
+import Ember from 'ember';
 
 moduleForComponent('hold-button', 'Integration | Component | hold button', {
   integration: true
 });
 
 test('it renders', function(assert) {
-  assert.expect(2);
+  assert.expect(4);
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{hold-button}}`);
-
-  assert.equal(this.$().text(), '');
+  this.render(hbs`{{hold-button delay=0 action='finished'}}`);
+  let $component = this.$('.ember-hold-button');
+  assert.equal($component.length, 1);
+  assert.ok(!$component.hasClass('is-holding'), "is-holding class not added");
+  assert.ok(!$component.hasClass('is-complete'), "is-complete class not added");
 
   // Template block usage:
   this.render(hbs`
@@ -24,4 +23,39 @@ test('it renders', function(assert) {
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
+});
+
+test('it calls the action', function(assert) {
+  assert.expect(5);
+  this.render(hbs`{{hold-button delay=0 action='finished'}}`);
+  let $component = this.$('.ember-hold-button');
+
+  let finished = false;
+  this.on('finished', function() {
+    finished = true;
+  });
+
+  $component.mousedown();
+  Ember.run.later(() => {
+    assert.ok($component.hasClass('is-holding'), "is-holding class added");
+    $component.mouseup();
+
+    Ember.run.later(() => {
+      assert.ok(!$component.hasClass('is-holding'), "is-holding class removed");
+      assert.ok($component.hasClass('is-complete'), "is-complete class added");
+      assert.ok(finished, "finished action called");
+
+      $component.mousedown();
+      Ember.run.later(() => {
+        assert.ok(!$component.hasClass('is-complete'), "is-complete class removed");
+      });
+    });
+  });
+});
+
+test('type reflects CSS class', function(assert) {
+  assert.expect(1);
+  this.render(hbs`{{hold-button delay=0 action='finished' type='banana'}}`);
+  let $component = this.$('.ember-hold-button');
+  assert.ok($component.hasClass('banana'), "type class added");
 });
